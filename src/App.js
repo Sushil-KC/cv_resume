1fd44888aec278ab1eea2a353273578ca5a4cdac
Sushil KC <sushilkc2060@gmail.com>
2024-11-27 16:44:09 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ParticlesComponent from './constants/Particles';
+import Introduction from './components/Introduction';
+import Educations from './components/Educations';
+import { Space } from './constants/Space';
+import Experience from './components/Experience';
+import Skills from './components/Skills';
+import Familar from './components/Familar';
+import DeveloperSkills from './components/Interest';
+import Projects from './components/Projects';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <ParticlesComponent />
+      <div className='min-h-screen py-6 px-3 text-black flex items-center justify-center'>
+        <div className='min-h-screen mx-auto w-full md:w-3/4 rounded-lg shadow-lg lg:w-2/3 xl:w-3/6 
+        bg-white/90 backdrop-blur-3xl border border-white/50'>
+          <div className='flex flex-row'>
+            <div className='p-12 w-full'>
+              <Introduction />
+              <Space />
+              <Educations />
+              <Space />
+              <Experience />
+              <Space />
+              <Skills />
+              <Space />
+              <Familar />
+              <Space />
+
+              <Projects />
+              <Space />
+              <DeveloperSkills />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+  )
 }
 
-export default App;
+export default App
---
+++ src/constants/Space.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Space = () => {
+  return (
+    <div className='py-2 flex justify-center'>
+        <div className='border-[1px] border-dashed border-gray-400 w-8'></div>
+    </div>
+  )
+}
